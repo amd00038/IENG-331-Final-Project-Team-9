e13d2804f8aad565f24903b899b1f08d494a0153
skt00004 <skt00004@mix.wvu.edu>
2025-04-23 14:20:14 -0400

    yay

--- q3_reviewer_trends.py
+++ q3_reviewer_trends.py
@@ -12,6 +12,24 @@ def _():
     import polars as pl
     import plotly.express as px
     import re
+    return (pl,)
+
+
+@app.cell
+def _(pl):
+    taster_name = (pl.read_parquet("pipeline/taster_name.parquet"))
+    taster_name
+    return (taster_name,)
+
+
+@app.cell
+def _(pl, taster_name):
+    counts = (
+            taster_name.with_columns(pl.col("province"))
+            .group_by("province")
+            .len().drop_nulls()
+        )
+    counts
     return
 
 
