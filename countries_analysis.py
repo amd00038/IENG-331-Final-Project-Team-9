8a60cf39423e492bfd8b4e8536b703ac56131846
amd00038 <amd00038@mix.wvu.edu>
2025-04-24 10:20:58 -0400

    commit

--- countries_analysis.py
+++ countries_analysis.py
@@ -3,7 +3,10 @@
 import marimo
 
 __generated_with = "0.13.0"
-app = marimo.App(width="medium")
+app = marimo.App(
+    width="medium",
+    layout_file="layouts/countries_analysis.slides.json",
+)
 
 
 @app.cell
@@ -12,13 +15,25 @@ def _():
     import polars as pl
     import plotly.express as px
     import re
-    return pl, px
+    return mo, pl, px
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""## Cleaned Data""")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""## I want this to go with the table""")
+    return
 
 
 @app.cell
 def _(pl):
-    countries_parquet = pl.read_parquet("pipeline/country.parquet")
-    countries_parquet
+    countries_parquet = pl.read_parquet("pipeline/country.parquet").drop_nulls(subset= pl.col("price"))
+    countries_parquet.head()
     return (countries_parquet,)
 
 
@@ -34,6 +49,16 @@ def _(countries_parquet, pl, px):
     number_wines = countries_parquet.unique(subset=pl.col("title")).group_by(pl.col("country")).agg(pl.len().alias("number of wines")).sort(by=pl.col("number of wines"), descending=True)
     px.bar(number_wines, x= "country", y = "number of wines")
     return
+
+
+app._unparsable_cell(
+    r"""
+    Country_points = countries_parquet.group_by(pl.col(\"country\")).agg(pl.col(\"points\").mean().alias(\"average points
+    \")).sort(by=pl.col(\"average price\"), descending=True)
+    px.bar(Country_price, x = \"country\", y=\"average points\")
+    """,
+    name="_"
+)
 
 
 if __name__ == "__main__":
