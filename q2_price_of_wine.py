5be887c5f19eb1d4e60dc1b158c8f99275ca07a8
amd00038 <amd00038@mix.wvu.edu>
2025-04-24 09:39:17 -0400

    Added box plot

--- q2_price_of_wine.py
+++ q2_price_of_wine.py
@@ -23,36 +23,54 @@ def _(mo):
 
 @app.cell
 def _(pl):
-    winedata = (
-            pl.read_csv("data/winemag-data.csv", try_parse_dates=True))
-    winedata
+    winedata = pl.read_parquet("pipeline/price.parquet")
+    winedata.head()
+
+
     return (winedata,)
 
 
 @app.cell
 def _(pl, winedata):
     result = winedata.select(pl.col("price", "points"))
-
-    result
     return
 
 
 @app.cell
 def _(px, winedata):
-    fig = px.scatter(
+    px.box(
         winedata, 
         x="points", 
         y="price",
-        title="Price of Wine vs Points Correlation"
-    )
-    fig
+        title="Price of Wine vs Points Correlation",range_y=[0,1500])
     return
 
 
 @app.cell
 def _(mo):
-    mo.md(r"""### Conclusion: The number of points does not predict the price of wine, as shown in the figure above, "Price of Wine vs Points Correlation". When color-coded using the ID, there is a clear visual that each point had a wide range of prices. For example, point 95 lowest price, is twenty dollars, and the highest price is nine hundred seventy-three dollars. There is a correlation between the range of prices versus each point. As shown above, points 80 through 86 do not have a wide price range compared to points 92 through 100. For this reason, the is a strong correlation in the fact that the higher the points, the wider the price range is for wine. """)
+    mo.md(r"""### Conclusion: The number of points does not predict the price of wine, as shown in the figure above, "Price of Wine vs Points Correlation". When color-coded using the ID, there is a clear visual that each point had a wide range of prices. For example, point 95 lowest price, is twenty dollars, and the highest price is nine hundred seventy-three dollars. There is a correlation between the range of prices versus each point. As shown above, points 80 through 86 do not have a wide price range compared to points 92 through 100. For this reason, the is a strong correlation in the fact that the higher the points, the wider the price range is for wine.""")
     return
+
+
+app._unparsable_cell(
+    r"""
+    corr_df = (
+            winedata.select(\"price\", \"humidity\", \"windspeed\", \"totalconsumption\")
+            .corr()
+        )
+
+        px.imshow(
+            corr_df,
+            x=[\"temperature\", \"humidity\", \"windspeed\", \"totalconsumption\"],
+            y=[\"totalconsumption\", \"windspeed\", \"humidity\", \"temperature\"],
+            text_auto=True,
+            zmax=1,
+            zmin=-1,
+            color_continuous_scale=\"RdBu_r\",
+        )
+    """,
+    name="_"
+)
 
 
 @app.cell
