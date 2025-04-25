040f99fa018478b0d2882c420d44233d6c82d198
amd00038 <amd00038@mix.wvu.edu>
2025-04-25 07:34:02 -0400

    q6 visualizations done

--- Project Questions.md
+++ Project Questions.md
@@ -4,7 +4,12 @@ Dataset containing ~130k wine reviews published to WineEnthusiast, including tas
 
 ## Recommended Analysis
 
-- Which province has the highest average price? How about of those with at least 10 wines?
+- Which province has the highest average price? How many of those with at least 10 wines?
 - Does the number of points predict the price of the wine? If so, how strong is the correlation?
 - Dig into reviewer-level trends. Do reviewers tend to specialize in one or two provinces? Do they specialize in certain varieties?
 - Dig into the 'description' column. What are some of the most common terms used in positive reviews? Negative reviews?
+
+## Additional Questions
+
+- What is the distribution of wine prices across different countries? How about the number of wines? Are there any countries that stand out in terms of the number of wines included or the average price? How does this compare to the average score of wines from those countries?
+- What are the distribution of years in the wines included? Are there any trends in the average score or price of wines over time?
--- q6_time_analysis.py
+++ q6_time_analysis.py
@@ -11,12 +11,54 @@ def _():
     import marimo as mo
     import polars as pl
     import plotly.express as px
-    return (pl,)
+    import datetime
+    return mo, pl, px
 
 
 @app.cell
 def _(pl):
-    times = pl.read_parquet("pipeline/")
+    times = (pl.read_parquet("pipeline/data.parquet")
+        .drop_nulls(subset=pl.col("title"))
+        .with_columns(pl.col("title").str.extract(r"(19\d{2}|20\d{2})").alias("Date").str.to_date(format = "%Y"))).drop_nulls(subset=pl.col("Date"))
+    times
+    return (times,)
+
+
+@app.cell
+def _(pl, times):
+    number_per_year = times.group_by("Date").agg(pl.col("title").len().alias("Number_of_wines")).sort(by="Number_of_wines",descending=True)
+    number_per_year.head(10)
+    return
+
+
+@app.cell
+def _(pl, times):
+    Low_per_year = times.group_by("Date").agg(pl.col("title").len().alias("Number_of_wines")).sort(by="Number_of_wines",descending=False)
+    Low_per_year.head(10)
+    return
+
+
+@app.cell
+def _(px, times):
+    px.box(times.sort(by = "Date", descending=False), x = "Date", y = "price", range_x = ["1998-08-01","2017-08-01"],range_y = [0,800])
+    return
+
+
+@app.cell
+def _(px, times):
+    px.box(times.sort(by = "Date", descending=False), x = "Date", y = "points", range_x = ["1998-08-01","2017-08-01"],range_y = [79,101])
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""### The box plots show that wines fromw within the last 26 years have a lot of variation when it comes to price. In particular there are a lot outliers. Now this is already a zoomed inview of the data as there are later wines cut off in addition to extreme outliers in price (around $3,000) that have been cutoff as well. """)
+    return
+
+
+@app.cell
+def _(pl, px, times):
+    px.bar(times.group_by("Date").agg(pl.col("price").mean().alias("Average Price")).sort(by="Date",descending=False), x="Date", y="Average Price",range_x = ["1998-08-01","2017-08-01"],range_y = [0,60])
     return
 
 
