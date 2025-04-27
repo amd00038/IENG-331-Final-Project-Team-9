f529e4a0de1e7532316512acf48348c043939965
skt00004 <skt00004@mix.wvu.edu>
2025-04-27 18:48:43 -0400

    yay

--- q3_reviewer_trends.py
+++ q3_reviewer_trends.py
@@ -11,39 +11,62 @@ def _():
     import marimo as mo
     import polars as pl
     import plotly.express as px
-    return pl, px
+    return mo, pl, px
 
 
 @app.cell
-def _(pl):
-    taster_name = pl.read_parquet("pipeline/taster_name.parquet").select(pl.col("taster_name"),pl.col("province")).drop_nulls(subset = pl.col("province"))
-    #taster_name
-    return (taster_name,)
+def _(mo):
+    mo.md(r"""## Taster Name and Province Data""")
+    return
 
 
 @app.cell
-def _(pl, taster_name):
-    split_df = taster_name.with_columns(pl.col("province").str.extract(r"^(.*?)(?:\s+(?:&|and)\s+|$)").alias("first_province"), pl.col("province").str.extract(r"(?:^.*?\s+(?:&|and)\s+)(.*)").alias("second_province"),
-        )
-
-    split_df
-    # split_df.null_count()
-    return (split_df,)
+def _():
+    # taster_name = pl.read_parquet("pipeline/taster_name.parquet").select(pl.col("taster_name"),pl.col("province")).drop_nulls(subset = pl.col("province"))
+    # #taster_name
+    return
 
 
 @app.cell
-def _(split_df):
-    two_province = split_df.drop_nulls("second_province")
-    reviewer_count_two_province = (
-            two_province.group_by("taster_name")
-            .len("second_province"))
+def _():
+    # split_df = taster_name.with_columns(pl.col("province").str.extract(r"^(.*?)(?:\s+(?:&|and)\s+|$)").alias("first_province"), pl.col("province").str.extract(r"(?:^.*?\s+(?:&|and)\s+)(.*)").alias("second_province"),
+    #     )
 
-    return (reviewer_count_two_province,)
+    # #split_df
+    return
 
 
 @app.cell
-def _(px, reviewer_count_two_province):
-    px.bar(reviewer_count_two_province, x="taster_name", y="second_province")
+def _(mo):
+    mo.md(r"""## Reviewer Province Count""")
+    return
+
+
+@app.cell
+def _():
+    # two_province = split_df.drop_nulls("second_province")
+    # reviewer_count_two_province = (
+    #         two_province.group_by("taster_name")
+    #         .len("second_province"))
+    # two_province
+    return
+
+
+@app.cell
+def _():
+    #px.bar(reviewer_count_two_province, x="taster_name", y="second_province")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""There are only 4 reviewers out of 20 that specialize in two provinces. The rest of the reviewers specialize in one province. The above bar chart visualizes this.""")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""## Reviewer Varieties Data""")
     return
 
 
@@ -55,6 +78,17 @@ def _(pl):
 
 
 @app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ## Reviewer Varieties Counts
+        The data frame below counts the number of unique varieties per taster.
+        """
+    )
+    return
+
+
+@app.cell
 def _(pl, varieties):
     taster_variety_counts = (
         varieties.group_by(['taster_name', 'designation'])
@@ -62,6 +96,12 @@ def _(pl, varieties):
     )
     #taster_variety_counts
     return (taster_variety_counts,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""The bar chart below visualizes the number of unique varieties per reviewer. This shows that majority of tasters reviewed a large variety of wines. They did not stick to specific varieties.""")
+    return
 
 
 @app.cell
