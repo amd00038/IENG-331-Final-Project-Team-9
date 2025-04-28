20e2d55dd05d7c4c4389e53833ea17cd156f5284
ecr00001 <ecr00001@mix.wvu.edu>
2025-04-28 18:22:46 -0400

    final

--- q2_price_of_wine.py
+++ q2_price_of_wine.py
@@ -25,14 +25,18 @@ def _(mo):
 def _(pl):
     winedata = pl.read_parquet("pipeline/price.parquet")
     winedata.head()
-
-
     return (winedata,)
 
 
 @app.cell
 def _(pl, winedata):
     result = winedata.select(pl.col("price", "points"))
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""## Price of Wine vs Points Correlation""")
     return
 
 
@@ -48,7 +52,7 @@ def _(px, winedata):
 
 @app.cell
 def _(mo):
-    mo.md(r"""### Conclusion: The number of points predicts the price of the wine based on the points of the wine shown above in the figure "Price of Wine vs Points Correlation". The graph shows a small expenential growth of the range of prices for prices up to 800. By creating a range to 800, the graph provides a clearer visual of the relation. For example, from points 80-90 there is a small range in prices. The minimum price for points 80-90 in from 32-90 dollars where as for points 90-100, the minimum price ranges from 80-90 dollars. The max price from 80-90 ranges from 69-510 dollars and from points 90-100 the max range is from 510-1500 dollars. The box plots shows an ezpential growth for the medium values for each point. """)
+    mo.md(r"""### Conclusion: The number of points predicts the price of wine based on the points shown above in the figure "Price of Wine vs Points Correlation". The graph shows small exponential growth as points increase up to 800. By creating a price range from 0-800, the graph provides a clearer visual of the relationship. For example, from points 80-90, there is a small price range. The minimum price for 80-90 points is 32-90 dollars, whereas for points 90-100, the minimum price ranges from 80-90 dollars. The max price from 80-90 ranges from 69-510 dollars, and from points 90-100 the max range is from 510-1500 dollars. The box plots show an exponential growth for the median values for each point and a larger variance as the points increase. """)
     return
 
 
